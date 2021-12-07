98458de6f1677b306084553b9a7622fe528fe710
hyunchanpark <eye3570@gmail.com>
2021-12-07 18:13:14 +0900

    Chore : export한 파일들 import 시 Component 에서 불러오게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { PageHeader } from 'Components/PageHeader';
 import styled from 'styled-components';
+import { PageHeader } from 'Components';
 
 const Container = styled.div``;
 
