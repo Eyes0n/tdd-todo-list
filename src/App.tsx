cff9ddc10c750b0451a6787224fb6ebd15520287
hyunchanpark <eye3570@gmail.com>
2021-12-08 15:11:02 +0900

    Feat : App 컴포넌트에 router적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Route, Switch } from 'react-router';
 import { PageHeader } from 'Components';
+import { List } from 'Pages';
 
 const Container = styled.div``;
 
@@ -8,6 +10,9 @@ function App() {
   return (
     <Container>
       <PageHeader />
+      <Switch>
+        <Route exact path="/" component={List} />
+      </Switch>
     </Container>
   );
 }
